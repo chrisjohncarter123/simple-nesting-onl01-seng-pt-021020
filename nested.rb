2cfdcbdf93418c19d56e0e7463ca2808b32daae0
Christopher Carter <chrisjohncarter123@gmail.com>
2020-02-17 03:54:01 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -83,7 +83,7 @@ def adding_matz
           :languages => ["C"]
         }
      }
-programmer_hash[]
+programmer_hash[:yukihiro_matsumoto] = {}
     
 end
 
@@ -106,7 +106,7 @@ def changing_alan
      #change what Alan Kay is :known_for to the value of the alans_new_info variable. 
      alans_new_info = "GUI"
      
-     
+     programmer_hash[:alan_kay][:known_for] = alans_new_info
 end
 
 def adding_to_dennis
@@ -126,4 +126,5 @@ def adding_to_dennis
         }
      }
 
+programmer_hash[:dennis_ritchie][:languages] << ""
 end
